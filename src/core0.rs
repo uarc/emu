e3d7c0fe1ef6e3f94798f68916f004961aa45fd4
Geordon Worley <vadixidav@gmail.com>
2016-03-21 10:29:56 -0400

    add more framework

--- src/core0.rs
+++ src/core0.rs
@@ -1,8 +1,44 @@
 extern crate nue;
-use super::Core;
+extern crate num;
+use super::{Core, SenderBus, ReceiverBus, make_bus};
 use std::io::Read;
 use std::sync::{Mutex, Arc, Barrier};
 use std::sync::mpsc::{Receiver, SyncSender, sync_channel};
+use std::ops::Deref;
+
+struct InBus<W> {
+    bus: ReceiverBus<W>,
+}
+
+struct OutBus<W> {
+    bus: SenderBus<W>,
+}
+
+#[derive(Default)]
+struct DStack<W> {
+    stack: Vec<W>,
+}
+
+impl<W> DStack<W> {
+    fn rotate(&mut self, pos: u8) {
+        let last = self.stack.len() - 1;
+        // TODO: Introduce debug on out of range
+        let v = self.stack.remove(last - pos as usize);
+        self.stack.push(v);
+    }
+
+    fn replace<F>(&mut self, c: F) where F: FnOnce(W) -> W {
+        let v = match self.stack.pop() {
+            Some(v) => v,
+            None => {
+                // TODO: Add proper debugging here
+                panic!("No value on stack to consume");
+            },
+        };
+
+        self.stack.push(c(v));
+    }
+}
 
 pub struct Core0<W> {
     running: bool,
@@ -14,9 +50,16 @@ pub struct Core0<W> {
     program: Vec<u8>,
     data: Vec<W>,
 
-    //Synchronization for receiving interrupts
+    // Incoming buses
+    incoming_buses: Vec<InBus<W>>,
+    outgoing_buses: Vec<OutBus<W>>,
+
+    // Synchronization for receiving interrupts
     interrupt_sender: Mutex<SyncSender<(W, Arc<Barrier>)>>,
     interrupt_receiver: Receiver<(W, Arc<Barrier>)>,
+
+    // Set up stack
+    dstack: DStack<W>,
 }
 
 impl Core0<u32> {
@@ -32,32 +75,51 @@ impl Core0<u32> {
             program: Vec::new(),
             data: vec![0; memory],
 
+            incoming_buses: Vec::new(),
+            outgoing_buses: Vec::new(),
+
             interrupt_sender: Mutex::new(sender),
             interrupt_receiver: receiver,
+
+            dstack: DStack::default(),
         }
     }
 }
 
 impl<W> Core<W> for Core0<W>
-    where W: nue::Decode
+    where W: Copy + num::Integer, usize: From<W>
 {
-    fn stream<R: Read>(&mut self, privilege: u8, address: u32, src: &mut R) {
+    fn append_buses<I>(&mut self, buses: I) where I: Iterator<Item=SenderBus<W>> {
 
     }
-    fn incept<R: Read>(&mut self, privilege: u8, address: u32, src: &mut R) {
 
+    fn aquire_bus(&mut self, index: usize) -> SenderBus<W> {
+        let buses = make_bus();
+        self.incoming_buses.push(InBus{
+            bus: buses.1
+        });
+        buses.0
     }
-    fn send(&mut self, privilege: u8, address: u32, value: W) {
-        let mut guard = self.interrupt_sender.lock().ok().unwrap();
-        let sender = &mut *guard;
-        let barrier = Arc::new(Barrier::new(2));
-        sender.send((value, barrier.clone()));
-        barrier.wait();
-    }
-    fn kill(&mut self, privilege: u8, address: u32) {
 
-    }
-    fn is_running(&self) -> bool {
-        false
+    fn begin(&mut self) {
+        // Get disjoint references so borrows can occur simultaneously
+        let dstack = &mut self.dstack;
+        let data = &mut self.data;
+        let prog = &mut self.program;
+        let pc = &mut self.pc;
+        let dcs = &mut self.dcs;
+        // Poll for any sort of communication
+
+        // Execute instruction
+        match prog[usize::from(*pc)] {
+            // rread#
+            x @ 0...3 => {
+                dstack.replace(|v| {
+                    data[usize::from(dcs[x as usize] + v)]
+                });
+            },
+            // TODO: Add all instructions
+            _ => {},
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,99 @@
+extern crate oncemutex as om;
 use std::io::Read;
+use std::sync::mpsc::{SyncSender, Receiver, sync_channel};
+use std::sync::{Mutex, Barrier};
+use om::OnceMutex;
 
 pub mod core0;
 
-/// Core is a trait that defines the standard UARC bus interface with a core for emulation purposes.
-/// Interacting with the core is synchronous and thread-safe. Beware of deadlock.
-///
-/// W is a word type for passing information along the bus.
+pub struct Permission {
+    privilege: u8,
+    address: u32,
+}
+
+pub struct Com<T> {
+    pub permission: Permission,
+    pub data: T,
+}
+
+impl<T> Com<T> {
+    fn new(permission: Permission, data: T) -> Self {
+        Com{
+            permission: permission,
+            data: data,
+        }
+    }
+}
+
+/// The emulated UARC synchronous bus
+pub struct SenderBus<W> {
+    // Send a stream to the target
+    stream: SyncSender<Com<OnceMutex<Box<Read>>>>,
+    // Incept the target
+    incept: SyncSender<Com<(Permission, OnceMutex<Box<Read>>)>>,
+    // Interrupt a target with a word
+    send: SyncSender<Com<W>>,
+    // Kill the target
+    kill: SyncSender<Com<()>>,
+}
+
+impl<W> SenderBus<W> {
+    /// Send a Read stream to the target through an Arc
+    fn stream(&self, c: Com<OnceMutex<Box<Read>>>) {
+        self.stream.send(c).ok().unwrap();
+    }
+
+    /// Send a Read stream and a fresh set of permissions through an Arc to incept a core
+    fn incept(&self, c: Com<(Permission, OnceMutex<Box<Read>>)>) {
+        self.incept.send(c).ok().unwrap();
+    }
+
+    /// Send a word to the target
+    fn send(&self, c: Com<W>) {
+        self.send.send(c).ok().unwrap();
+    }
+
+    /// Send a kill signal to the target
+    fn kill(&self, c: Com<()>) {
+        self.kill.send(c).ok().unwrap();
+    }
+}
+
+/// The pair of the Bus which is defined to avoid duplication
+pub struct ReceiverBus<W> {
+    pub stream: Receiver<Com<OnceMutex<Box<Read>>>>,
+    pub incept: Receiver<Com<(Permission, OnceMutex<Box<Read>>)>>,
+    pub send: Receiver<Com<W>>,
+    pub kill: Receiver<Com<()>>,
+}
+
+pub fn make_bus<W>() -> (SenderBus<W>, ReceiverBus<W>) {
+    let streams = sync_channel(0);
+    let incepts = sync_channel(0);
+    let sends = sync_channel(0);
+    let kills = sync_channel(0);
+
+    (SenderBus{
+        stream: streams.0,
+        incept: incepts.0,
+        send: sends.0,
+        kill: kills.0,
+    }, ReceiverBus{
+        stream: streams.1,
+        incept: incepts.1,
+        send: sends.1,
+        kill: kills.1,
+    })
+}
+
+/// Core is a trait that cores implement to allow Bus to be created connecting cores
 pub trait Core<W> {
-    /// The privilege/address is for the core sending.
-    fn stream<R: Read>(&mut self, privilege: u8, address: u32, src: &mut R);
-    /// The privilege/address is for the core receiving.
-    fn incept<R: Read>(&mut self, privilege: u8, address: u32, src: &mut R);
-    /// The privilege/address is for the core sending.
-    fn send(&mut self, privilege: u8, address: u32, value: W);
-    /// This wont complete until it succeeds.
-    /// The privilege/address is for the core sending.
-    fn kill(&mut self, privilege: u8, address: u32);
-    /// This only can determine if the core was running or not, but it is not synchronous.
-    fn is_running(&self) -> bool;
+    /// Set the internal buses
+    fn append_buses<I>(&mut self, buses: I) where I: Iterator<Item=SenderBus<W>>;
+    /// Aquire a bus located at a particular bus ID
+    fn aquire_bus(&mut self, index: usize) -> SenderBus<W>;
+
+    /// Begins operation in the current thread
+    /// Killing the core will not end the thread.
+    fn begin(&mut self);
 }
